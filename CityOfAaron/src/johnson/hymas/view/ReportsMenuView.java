7b6ea68258708f8b505db90afa162d8a6a7f02b6
Kristy <Kristy@192.168.0.8>
2018-06-25 15:41:59 -0500

    06-14-18

--- CityOfAaron/src/johnson/hymas/view/ReportsMenuView.java
+++ CityOfAaron/src/johnson/hymas/view/ReportsMenuView.java
@@ -2,82 +2,35 @@ package johnson.hymas.view;
 
 import java.util.Scanner;
 
+
 /**
  *
  * @author lando
  */
-public class ReportsMenuView {
+public class ReportsMenuView extends ViewBase {
 
-    /**
-     * The message that will be displayed by this view.
-     */
-    protected String message;
+   
 
     /**
      * Constructor
      */
     public ReportsMenuView() {
 
-        message = "Reports Menu\n"
-                + "---------\n"
-                + "V - View Authors\n"
-                + "A - View Animals\n"
-                + "T - View Tools\n"
-                + "P - View Provisions"
-                + "M - Return Main Menu";
     }
 
-    /**
-     * Get the user's input. Keep prompting them until they enter a value.
-     *
-     * @param prompt
-     * @param allowEmpty - determine whether the user can enter no value (just a
-     * return key)
-     * @return
-     */
-    protected String getUserInput(String prompt, boolean allowEmpty) {
-
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
-        boolean inputReceived = false;
-
-        while (inputReceived == false) {
-
-            System.out.println(prompt);
-            input = keyboard.nextLine();
-
-            // Make sure we avoid a null-pointer error.
-            if (input == null) {
-                input = "";
-            }
-
-            // Trim any trailing whitespace, including the carriage return.
-            input = input.trim();
-
-            if (input.equals("") == false || allowEmpty == true) {
-                inputReceived = true;
-            }
-        }
-
-        return input;
+    @Override
+    protected String getMessage(){
+        
     }
 
-    /**
-     * An overloaded version of getUserInput that sets allowEmpty to false so we
-     * don't have to type it ourselves.
-     *
-     * @param prompt
-     * @return
-     */
-    protected String getUserInput(String prompt) {
-        return getUserInput(prompt, false);
-    }
+    
 
     /**
      * Get the set of inputs from the user.
      *
      * @return
      */
+    @Override
     public String[] getInputs() {
 
         String[] inputs = new String[1];
@@ -93,6 +46,7 @@ public class ReportsMenuView {
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
+    @Override
     public boolean doAction(String[] inputs) {
 
         switch (inputs[0].trim().toUpperCase()) {
@@ -117,21 +71,7 @@ public class ReportsMenuView {
         return true;
     }
 
-    /**
-     * Control this view's display/prompt/action loop until the user chooses and
-     * action that causes this view to close.
-     */
-    public void displayView() {
-
-        boolean keepGoing = true;
-
-        while (keepGoing == true) {
-
-            System.out.println(message);
-            String[] inputs = getInputs();
-            keepGoing = doAction(inputs);
-        }
-    }
+    
 
     private void showReports() {
         System.out.println("showReports Function Called and coming soon, returning to Game Menu");
