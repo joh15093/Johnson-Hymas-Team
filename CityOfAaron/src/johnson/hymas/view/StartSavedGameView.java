a65ec16bc5fd5ed13672671191cec16ee6338ca3
joh15 <joh15@192.168.0.22>
2018-07-12 21:41:20 -0500

    7-12-2018

---
+++ CityOfAaron/src/johnson/hymas/view/StartSavedGameView.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package johnson.hymas.view;
+
+/**
+ *
+ * @author joh15
+ */
+public class StartSavedGameView {
+    
+private String[] getInputs()  {
+    inputs = new String array one element long
+            
+    Display instructions on how to get your saved game
+    input1 = getInput (thePromptMessage)
+    save a reference to input1 in the inputs array
+    return inputs        
+}
+    
+}
